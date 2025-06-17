f5ff9fe1c96f22b6999d46c6d0ce664a8219a1af
awais <awaisn.offi@gmail.com>
2025-06-17 04:56:54 +0000

    yes

--- MiddlesWares/middlewares.js
+++ MiddlesWares/middlewares.js
@@ -5,8 +5,8 @@ const middleWares = (app)=>{
     app.use(express.json())
     app.use(express.urlencoded({ extended: true }))
     app.use(cors({
-    origin: 'http://localhost:5173', 
-    credentials: true,
+    origin: '*', 
+    // credentials: true,
   }))   
-}
+} 
 export default middleWares
